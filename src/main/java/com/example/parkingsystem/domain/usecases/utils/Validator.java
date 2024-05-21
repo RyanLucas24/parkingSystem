7e836b6da570f2f61d114308364944d16fa7526e
Ryan Lucas <ryanlucaspatron@gmail.com>
2024-05-20 22:07:18 -0300

    Feat: Criação da classe abstrata Validator com métodos de validação de informações seguindo padrão Symploteca

---
+++ src/main/java/com/example/parkingsystem/domain/usecases/utils/Validator.java
@@ -0,0 +1,16 @@
+package com.example.parkingsystem.domain.usecases.utils;
+
+import java.util.Collection;
+
+public abstract class Validator<T>{
+    public abstract Notification validate(T type);
+    public static boolean nullOrEmpty(String string){
+        return string == null || string.isEmpty();
+    }
+    public static boolean nullOrEmpty(Collection collection){
+        return collection == null || collection.isEmpty();
+    }
+    public static boolean negative(int integer){
+        return integer < 0;
+    }
+}
