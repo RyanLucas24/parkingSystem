ec0fbec95eb268bc49433ce301229562405ff74f
Ryan Lucas <ryanlucaspatron@gmail.com>
2024-05-21 00:36:05 -0300

    Feat: criação do método de verificação se há objetos nulos ou vazios na lista de placas na classe ClientBasicInformationInputRequestValidator

--- src/main/java/com/example/parkingsystem/domain/usecases/client/ClientBasicInformationInputRequestValidator.java
+++ src/main/java/com/example/parkingsystem/domain/usecases/client/ClientBasicInformationInputRequestValidator.java
@@ -4,6 +4,9 @@ import com.example.parkingsystem.domain.model.client.Client;
 import com.example.parkingsystem.domain.usecases.utils.Notification;
 import com.example.parkingsystem.domain.usecases.utils.Validator;
 
+import java.util.Collection;
+import java.util.List;
+
 public class ClientBasicInformationInputRequestValidator extends Validator<Client> {
     @Override
     public Notification validate(Client client) {
@@ -12,12 +15,18 @@ public class ClientBasicInformationInputRequestValidator extends Validator<Clien
             notification.addError("Cliente está nulo!!");
             return notification;
         }
-        if (nullOrEmpty(client.getCpf())) {
+        if (nullOrEmpty(client.getCpf()))
             notification.addError("CPF está nulo ou vazio!!");
-        }
-        if (nullOrEmpty(client.getVehiclesPlate())){
-            notification.addError("Placas do Veículo estão nulas ou vazias!!");
-        }
+        if(hasANullOrEmptyElement(client.getVehiclesPlate()))
+            notification.addError("Existem placas de veículos nulas ou vazias!!");
         return notification;
+    }
+
+    public static boolean hasANullOrEmptyElement(Collection<String> collection){
+        List<String> list = collection.stream()
+                .filter(Validator::nullOrEmpty)
+                .toList();
+
+        return !list.isEmpty();
     }
 }
