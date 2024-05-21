8254626d319ac283b3be97d6fd37d9c5b1a3b4c1
Ryan Lucas <ryanlucaspatron@gmail.com>
2024-05-20 22:14:05 -0300

    Feat: Criação das classes de excessão EntityAlreadyExistsException e EntityNotFoundException seguindo padrão Symploteca

---
+++ src/main/java/com/example/parkingsystem/domain/usecases/utils/EntityAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.example.parkingsystem.domain.usecases.utils;
+
+public class EntityAlreadyExistsException extends RuntimeException{
+    public EntityAlreadyExistsException(String message) {
+        super(message);
+    }
+}
