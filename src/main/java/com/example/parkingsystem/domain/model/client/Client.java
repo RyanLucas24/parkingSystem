39caa46da0049e3a46296b1004c54d0518570576
Ryan Lucas <ryanlucaspatron@gmail.com>
2024-05-20 22:33:49 -0300

    Feat: Criação de Getters, Setters, Construtor básico da classe Client e retirada de atributos CheckIn e CheckOut

--- src/main/java/com/example/parkingsystem/domain/model/client/Client.java
+++ src/main/java/com/example/parkingsystem/domain/model/client/Client.java
@@ -8,9 +8,60 @@ public class Client {
     private String phone;
     private String email;
     private String address;
-    private Date checkIn;
-    private Date checkOut;
-    private int rentedSpace;
     private String vehiclePlate;
 
+    public Client(String cpf, String vehiclePlate) {
+        this.cpf = cpf;
+        this.vehiclePlate = vehiclePlate;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getVehiclePlate() {
+        return vehiclePlate;
+    }
+
+    public void setVehiclePlate(String vehiclePlate) {
+        this.vehiclePlate = vehiclePlate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
 }
+
