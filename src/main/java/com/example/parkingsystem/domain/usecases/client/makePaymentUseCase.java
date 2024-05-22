830498c7857e24aa6fa443f530ffa727967950dc
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-05-22 12:07:01 -0300

    feat: create constructor

--- src/main/java/com/example/parkingsystem/domain/usecases/client/makePaymentUseCase.java
+++ src/main/java/com/example/parkingsystem/domain/usecases/client/makePaymentUseCase.java
@@ -1,7 +1,17 @@
 package com.example.parkingsystem.domain.usecases.client;
 
 import com.example.parkingsystem.domain.model.client.Client;
+import com.example.parkingsystem.domain.usecases.payment.PaymentDAO;
 
-public class makePaymentUseCase (PaymentDao paymentDao, ClientDAO clientDAO){
+import java.util.Optional;
 
+
+public class makePaymentUseCase{
+    private PaymentDAO paymentDAO;
+    private ClientDAO clientDAO;
+
+    public makePaymentUseCase(PaymentDAO paymentDAO, ClientDAO clientDAO) {
+        this.paymentDAO = paymentDAO;
+        this.clientDAO = clientDAO;
+    }
 }
