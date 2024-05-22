f0f83c5d116f09df0374b1c2752d3b45c101ffc8
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-05-22 12:31:40 -0300

    refactor: change date from type Date to LocalDateTime, create getters/setters and constructor

--- src/main/java/com/example/parkingsystem/domain/model/payment/Payment.java
+++ src/main/java/com/example/parkingsystem/domain/model/payment/Payment.java
@@ -1,9 +1,40 @@
 package com.example.parkingsystem.domain.model.payment;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 public class Payment {
     private double value;
-    private paymentMethodEnum paymentMethod;
-    private Date date;
+    private PaymentMethodEnum paymentMethod;
+    private LocalDateTime date;
+
+    public Payment(double value, PaymentMethodEnum paymentMethod) {
+        this.value = value;
+        this.paymentMethod = paymentMethod;
+        this.date = LocalDateTime.now();
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public PaymentMethodEnum getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(PaymentMethodEnum paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = LocalDateTime.now();
+    }
 }
