cd88970a995a94b5fb0a20b99cb3b849e963cdb5
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-05-22 12:30:24 -0300

    feat: create calcularDuracao method

--- src/main/java/com/example/parkingsystem/domain/usecases/client/makePaymentUseCase.java
+++ src/main/java/com/example/parkingsystem/domain/usecases/client/makePaymentUseCase.java
@@ -1,8 +1,11 @@
 package com.example.parkingsystem.domain.usecases.client;
 
 import com.example.parkingsystem.domain.model.client.Client;
+import com.example.parkingsystem.domain.model.payment.*;
 import com.example.parkingsystem.domain.usecases.payment.PaymentDAO;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 
@@ -13,5 +16,11 @@ public class makePaymentUseCase{
     public makePaymentUseCase(PaymentDAO paymentDAO, ClientDAO clientDAO) {
         this.paymentDAO = paymentDAO;
         this.clientDAO = clientDAO;
+    }
+
+    public long calcularDuracao(LocalDateTime dataInicial) {
+        LocalDateTime dataFinal = LocalDateTime.now();
+        Duration d = Duration.between(dataInicial, dataFinal);
+        return d.toMinutes();
     }
 }
