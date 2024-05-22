65dafa4b70cc7976e39cff8ea37668213e461ec2
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-05-22 11:56:49 -0300

    chore: create makePaymentUseCase file

---
+++ src/main/java/com/example/parkingsystem/domain/usecases/client/makePaymentUseCase.java
@@ -0,0 +1,7 @@
+package com.example.parkingsystem.domain.usecases.client;
+
+import com.example.parkingsystem.domain.model.client.Client;
+
+public class makePaymentUseCase (PaymentDao paymentDao, ClientDAO clientDAO){
+
+}
