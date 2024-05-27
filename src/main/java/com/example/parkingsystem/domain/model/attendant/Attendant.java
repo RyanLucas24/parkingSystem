17a2fdcd151dccc39a3cea1879b96173b7f6a654
Willian Cristian <44729804+wcristiansilva@users.noreply.github.com>
2024-05-26 21:55:28 -0300

    Modificado Manager add basic diagrams

--- src/main/java/com/example/parkingsystem/domain/model/attendant/Attendant.java
+++ src/main/java/com/example/parkingsystem/domain/model/attendant/Attendant.java
@@ -18,39 +18,39 @@ public class Attendant {
         if(availableVacancies > 0){
             clients.add(client);
             availableVacancies--;
-            System.out.println("Entry registered:" + client);
+            System.out.println("Entrada registrada:" + client);
         } else {
-            System.out.println("No vacancies available");
+            System.out.println("Não há vagas disponíveis");
         }
     }
 
     public void registerExit(Client client) {
         if (clients.remove(client)){
             availableVacancies++;
-            System.out.println("Exit registered:" + client);
+            System.out.println("Saída registrada:" + client);
         } else {
-            System.out.println("Client not found");
+            System.out.println("Cliente não encontrado");
         }
     }
 
     public void registerContactDetails(Client client){
         // adicionar lógica para atualizar dados de contato se necessário
 
-        System.out.println("Contact details registered:" + client);
+        System.out.println("Detalhes de contato registrados:" + client);
     }
 
     public void makeThePayment(Client client){
         // Aqui adicionar lógica para efetuar pagamento
-        System.out.println("Payment made:" + client);
+        System.out.println("Pagamento realizado:" + client);
     }
 
     public void chargeAdditionalAmount(Client client){
         // Aqui adicionar lógica para cobrar valor adicional
-        System.out.println("Additional value charged:" + client);
+        System.out.println("Valor adicional cobrado:" + client);
     }
 
     public void gerenciarVagas(){
         // Aqui adicionar lógica para gerenciar vagas
-        System.out.println("Vacancies managed");
+        System.out.println("Vagas gerenciadas");
     }
 }
