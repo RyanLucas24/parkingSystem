efc9862c54dde35584a321587a58f65a8e609359
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-06-05 12:13:41 -0300

    chore: create test file

---
+++ src/test/java/com/example/parkingsystem/domain/usecases/client/MakePaymentUseCaseTest.java
@@ -0,0 +1,14 @@
+package com.example.parkingsystem.domain.usecases.client;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+
+public class MakePaymentUseCaseTest {
+    @Test
+    @DisplayName("Test display name")
+    void testDisplayName() {
+        Assertions.fail();
+    }
+}
