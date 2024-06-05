43c041502e9a2f3aa0c232d942f0e618cee11464
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-06-05 17:56:51 -0300

    style: erase unused instances.

--- src/main/java/com/example/parkingsystem/domain/usecases/payment/MakePaymentUseCase.java
+++ src/main/java/com/example/parkingsystem/domain/usecases/payment/MakePaymentUseCase.java
@@ -3,16 +3,14 @@ package com.example.parkingsystem.domain.usecases.payment;
 import com.example.parkingsystem.domain.model.client.Client;
 import com.example.parkingsystem.domain.model.payment.Payment;
 import com.example.parkingsystem.domain.model.payment.PaymentMethodEnum;
-import com.example.parkingsystem.domain.usecases.client.ClientDAO;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 
-
 public class MakePaymentUseCase {
 
     public Payment makePayment (Client client, PaymentMethodEnum paymentMethod) {
-        if (client == null) return null;
+        if (client.getEntryDate() == null) return null;
         double pricePerMinute = 0.2;
         double totalPaymentValue = pricePerMinute * calcularDuracao(client);
         return new Payment(totalPaymentValue, paymentMethod);
