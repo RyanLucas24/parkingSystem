1dc3d96ec19d3e49cde9775bd483f30702a4264f
Ryan Lucas <ryanlucaspatron@gmail.com>
2024-06-05 21:01:05 -0300

    Refactor: Criação do construtor recebendo os DAOs necessários e atualização do método makePayment

--- src/main/java/com/example/parkingsystem/domain/usecases/payment/MakePaymentUseCase.java
+++ src/main/java/com/example/parkingsystem/domain/usecases/payment/MakePaymentUseCase.java
@@ -3,23 +3,35 @@ package com.example.parkingsystem.domain.usecases.payment;
 import com.example.parkingsystem.domain.model.client.Client;
 import com.example.parkingsystem.domain.model.payment.Payment;
 import com.example.parkingsystem.domain.model.payment.PaymentMethodEnum;
+import com.example.parkingsystem.domain.model.service.MonthlyService;
+import com.example.parkingsystem.domain.model.service.Service;
+import com.example.parkingsystem.domain.model.service.StandardService;
+import com.example.parkingsystem.domain.usecases.client.ClientDAO;
+import com.example.parkingsystem.domain.usecases.service.ServiceDAO;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class MakePaymentUseCase {
-
-    public Payment makePayment (Client client, PaymentMethodEnum paymentMethod) {
-        if (client.getEntryDate() == null) return null;
-        double pricePerMinute = 0.2;
-        double totalPaymentValue = pricePerMinute * calcularDuracao(client);
-        return new Payment(totalPaymentValue, paymentMethod);
+    private ServiceDAO serviceDAO;
+    private PaymentDAO paymentDAO;
+    private ClientDAO clientDAO;
+    public MakePaymentUseCase(ServiceDAO serviceDAO, PaymentDAO paymentDAO, ClientDAO clientDAO){
+        this.paymentDAO = paymentDAO;
+        this.serviceDAO = serviceDAO;
+        this.clientDAO = clientDAO;
     }
 
-    private double calcularDuracao(Client client) {
-        LocalDateTime dataInicial = client.getEntryDate();
-        LocalDateTime dataFinal = LocalDateTime.now();
-        Duration d = Duration.between(dataInicial, dataFinal);
-        return d.toMinutes();
+    public boolean makePayment(Client client, PaymentMethodEnum paymentMethod) {
+        Service service = client.getService();
+        Payment payment = new Payment(service.calculateBilling(), paymentMethod);
+        paymentDAO.create(payment);
+
+        if(service instanceof MonthlyService){
+            ((MonthlyService) service).setPaymentChecked(true);
+            serviceDAO.update(service);
+            clientDAO.update(client);
+        }
+        return true;
     }
 }
