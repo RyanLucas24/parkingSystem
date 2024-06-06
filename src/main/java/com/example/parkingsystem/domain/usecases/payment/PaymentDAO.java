dce41b5f01719ed4fdd1213b2d40c41dd5769357
Ryan Lucas <ryanlucaspatron@gmail.com>
2024-06-05 21:06:29 -0300

    Refactor: Troca do tipo genérico DAO da classe PaymentDAO de String para Integer

--- src/main/java/com/example/parkingsystem/domain/usecases/payment/PaymentDAO.java
+++ src/main/java/com/example/parkingsystem/domain/usecases/payment/PaymentDAO.java
@@ -3,5 +3,5 @@ package com.example.parkingsystem.domain.usecases.payment;
 import com.example.parkingsystem.domain.model.payment.Payment;
 import com.example.parkingsystem.domain.usecases.utils.DAO;
 
-public interface PaymentDAO extends DAO<Payment, String> {
+public interface PaymentDAO extends DAO<Payment, Integer> {
 }
