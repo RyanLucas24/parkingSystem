6a5e93653310c653a30d4b75b789b57151d99db8
Erick Gomes <119770693+ericksgmes@users.noreply.github.com>
2024-06-25 16:30:21 -0300

    refactor: remove unused variables.

--- src/main/java/com/example/parkingsystem/domain/model/service/Service.java
+++ src/main/java/com/example/parkingsystem/domain/model/service/Service.java
@@ -5,11 +5,6 @@ import com.example.parkingsystem.domain.model.client.Client;
 import java.time.LocalDateTime;
 
 public abstract class Service {
-    private double value;
-    private double time;
-    private double toleranceTime;
-    private Client client;
-
     public abstract double calculateBilling(LocalDateTime dataInicial);
 
     public double pricePerMinute() {
