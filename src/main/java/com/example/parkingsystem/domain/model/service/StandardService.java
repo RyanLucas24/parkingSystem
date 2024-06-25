68eb520aca1f7a26da4abc7acb12823e612ac48c
Erick Gomes <119770693+ericksgmes@users.noreply.github.com>
2024-06-25 16:31:47 -0300

    refactor: create constructor, calcularDuracao method, change overrideBilling logic, override pricePerMinute method.

--- src/main/java/com/example/parkingsystem/domain/model/service/StandardService.java
+++ src/main/java/com/example/parkingsystem/domain/model/service/StandardService.java
@@ -2,15 +2,32 @@ package com.example.parkingsystem.domain.model.service;
 
 import com.example.parkingsystem.domain.model.client.Client;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 public class StandardService extends Service{
-    private double additionalValue;
-    private Date checkIn;
-    private Date checkOut;
+    private final double additionalValue;
 
     @Override
-    public double calculateBilling() {
-        return 0;
+    public double pricePerMinute(){
+        return 0.3;
+    }
+
+    @Override
+    public double calculateBilling(LocalDateTime dataInicial) {
+        double minutosPermanecidos = calcularDuracao(dataInicial);
+        return (minutosPermanecidos * pricePerMinute()) + additionalValue;
+    }
+
+    public StandardService(double additionalValue, LocalDateTime checkIn) {
+        this.additionalValue = additionalValue;
+        LocalDateTime checkOut = LocalDateTime.now();
+    }
+
+    private double calcularDuracao(LocalDateTime dataInicial) {
+        LocalDateTime dataFinal = LocalDateTime.now();
+        Duration d = Duration.between(dataInicial, dataFinal);
+        return d.toMinutes();
     }
 }
