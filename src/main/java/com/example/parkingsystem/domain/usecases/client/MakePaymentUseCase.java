7f5e089cfe40cf6f1d3433ffe2fc45252589bcdd
Erick Gomes <119770693+ericksgmes@users.noreply.github.com>
2024-06-25 16:32:19 -0300

    refactor: use StandardService price calculations.

--- src/main/java/com/example/parkingsystem/domain/usecases/client/MakePaymentUseCase.java
+++ src/main/java/com/example/parkingsystem/domain/usecases/client/MakePaymentUseCase.java
@@ -3,30 +3,17 @@ package com.example.parkingsystem.domain.usecases.client;
 import com.example.parkingsystem.domain.model.client.Client;
 import com.example.parkingsystem.domain.model.payment.Payment;
 import com.example.parkingsystem.domain.model.payment.PaymentMethodEnum;
-import com.example.parkingsystem.domain.usecases.payment.PaymentDAO;
+import com.example.parkingsystem.domain.model.service.StandardService;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 
-
 public class MakePaymentUseCase {
-    private PaymentDAO paymentDAO;
-    private ClientDAO clientDAO;
-
-    public MakePaymentUseCase(PaymentDAO paymentDAO, ClientDAO clientDAO) {
-        this.paymentDAO = paymentDAO;
-        this.clientDAO = clientDAO;
-    }
-
-    private double calcularDuracao(LocalDateTime dataInicial) {
-        LocalDateTime dataFinal = LocalDateTime.now();
-        Duration d = Duration.between(dataInicial, dataFinal);
-        return d.toMinutes();
-    }
+    StandardService standardService;
 
     public Payment payment(Client client, PaymentMethodEnum paymentMethod) {
-        double tempoEstacionado = calcularDuracao(client.getEntryDate());
-        double precoPorMinuto = 0.2;
-        return new Payment((tempoEstacionado*precoPorMinuto), paymentMethod);
+        standardService = new StandardService(0, client.getEntryDate());
+        double value = standardService.calculateBilling(client.getEntryDate());
+        return new Payment(value, paymentMethod);
     }
 }
