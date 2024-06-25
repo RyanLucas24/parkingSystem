1a7e13a781493ec0b327868b70e011ddce7bd562
Erick Gomes <119770693+ericksgmes@users.noreply.github.com>
2024-06-25 16:35:02 -0300

    refactor: change calculateBilling to conform to Service refactor.

--- src/test/java/com/example/parkingsystem/domain/usecases/client/MakePaymentUseCaseTest.java
+++ src/test/java/com/example/parkingsystem/domain/usecases/client/MakePaymentUseCaseTest.java
@@ -1,14 +1,46 @@
 package com.example.parkingsystem.domain.usecases.client;
 
-import org.junit.jupiter.api.DisplayName;
+import com.example.parkingsystem.domain.model.client.Client;
+import com.example.parkingsystem.domain.model.payment.Payment;
+import com.example.parkingsystem.domain.model.payment.PaymentMethodEnum;
+import org.junit.jupiter.api.*;
+import org.mockito.Mock;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.clearInvocations;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+
+import java.time.LocalDateTime;
 
 public class MakePaymentUseCaseTest {
+    @BeforeEach
+    void setUp() {
+        MakePaymentUseCase makePayment;
+
+    }
+    
     @Test
-    @DisplayName("Test display name")
+    @DisplayName("Should create a new payment")
     void testDisplayName() {
-        Assertions.fail();
+        Client client = new Client("123", "a123");
+        MakePaymentUseCase makePayment = new MakePaymentUseCase();
+        client.setEntryDate(LocalDateTime.parse("2024-05-24T08:00:00.000"));
+        Payment payment = makePayment.payment(client, PaymentMethodEnum.CARTAO);
+        Assertions.assertNotNull(payment);
+    }
+
+    @Test
+    @DisplayName("Should create correct stay time")
+    void shouldCreateCorrectStayTime() {
+        Client client = new Client("123", "a123");
+        MakePaymentUseCase makePayment = new MakePaymentUseCase();
+        LocalDateTime tenMinuteEntryDate = LocalDateTime.now().minusMinutes(60);
+        client.setEntryDate(tenMinuteEntryDate);
+        Payment payment = makePayment.payment(client, PaymentMethodEnum.CARTAO);
+        Assertions.assertEquals( 18,payment.getValue());
     }
 }
