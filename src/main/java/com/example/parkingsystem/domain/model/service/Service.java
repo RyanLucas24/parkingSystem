aea92f006df3701b991e7a142bb83ef24a8964a0
Erick Gomes <119770693+ericksgmes@users.noreply.github.com>
2024-06-25 16:29:35 -0300

    refactor: add initialDate to calculateBilling and create pricePerMinute method

--- src/main/java/com/example/parkingsystem/domain/model/service/Service.java
+++ src/main/java/com/example/parkingsystem/domain/model/service/Service.java
@@ -2,11 +2,17 @@ package com.example.parkingsystem.domain.model.service;
 
 import com.example.parkingsystem.domain.model.client.Client;
 
+import java.time.LocalDateTime;
+
 public abstract class Service {
     private double value;
     private double time;
     private double toleranceTime;
     private Client client;
 
-    public abstract double calculateBilling();
+    public abstract double calculateBilling(LocalDateTime dataInicial);
+
+    public double pricePerMinute() {
+        return 0;
+    }
 }
