da07813297011a98cf774481e6cb4e44f594076c
Erick Gomes <119770693+ericksgmes@users.noreply.github.com>
2024-06-26 11:17:41 -0300

    feat: monthlyPayment method.

--- src/main/java/com/example/parkingsystem/domain/usecases/client/MakePaymentUseCase.java
+++ src/main/java/com/example/parkingsystem/domain/usecases/client/MakePaymentUseCase.java
@@ -3,14 +3,22 @@ package com.example.parkingsystem.domain.usecases.client;
 import com.example.parkingsystem.domain.model.client.Client;
 import com.example.parkingsystem.domain.model.payment.Payment;
 import com.example.parkingsystem.domain.model.payment.PaymentMethodEnum;
+import com.example.parkingsystem.domain.model.service.MonthlyService;
 import com.example.parkingsystem.domain.model.service.StandardService;
 
 public class MakePaymentUseCase {
     StandardService standardService;
+    MonthlyService monthlyService;
 
     public Payment payment(Client client, PaymentMethodEnum paymentMethod) {
         standardService = new StandardService(0, client.getEntryDate());
         double value = standardService.calculateBilling(client.getEntryDate());
+        return new Payment(value, paymentMethod);
+    }
+
+    public Payment monthlyPayment(Client client, PaymentMethodEnum paymentMethod) {
+        monthlyService = new MonthlyService(false);
+        double value = monthlyService.calculateBilling(client.getEntryDate());
         return new Payment(value, paymentMethod);
     }
 }
