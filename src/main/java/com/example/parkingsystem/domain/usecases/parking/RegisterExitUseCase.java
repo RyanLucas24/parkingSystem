a95f4570b80c9a2d120f7ea9168febfdda46fe3f
Erick Gomes <119770693+ericksgmes@users.noreply.github.com>
2024-06-26 14:25:28 -0300

    style: comment function that is not implemented.

--- src/main/java/com/example/parkingsystem/domain/usecases/parking/RegisterExitUseCase.java
+++ src/main/java/com/example/parkingsystem/domain/usecases/parking/RegisterExitUseCase.java
@@ -26,45 +26,45 @@ public class RegisterExitUseCase {
         this.clientDAO = clientDAO;
     }
 
-    public Client getClientByCpf(String cpf){
-        Optional<Client> clientOptional = clientDAO.findOne(cpf);
-        if(clientOptional.isEmpty()) {
-            throw new IllegalArgumentException("Cliente não encontrado");
-        }
-
-        Client clientFound = clientOptional.get();
-        MonthlyService monthlyService = (MonthlyService) clientFound.getService();
-        StandardService standardService = (StandardService) clientFound.getService();
-
-        switch (clientFound.getService()) {
-            case "standard": // TODO - Refatorar para usar o enum ou trocar para string
-                // Chama o cálculo de valor do pagamento para serviço standard
-                standardService.setCheckOut(LocalDateTime.now());
-                standardService.calculateBilling();
-                setPaymentChecked(true);
-                if (isPaymentChecked()) {
-                    // Libera a vaga e o cliente
-                    parking.liberateAStandardParkingSpace();
-                } else {
-                    throw new IllegalArgumentException("Pagamento não efetuado");
-                }
-                break;
-            // fluxo alternativo 1
-            case "monthly": // TODO - Refatorar para usar o enum ou trocar para string
-                if (monthlyService.isPaymentChecked()) {
-                    // Libera a vaga e o cliente
-                    parking.liberateAMonthlyParkingSpace();
-                } else {
-                    throw new IllegalArgumentException("Pagamento não efetuado");
-                }
-                break;
-
-            default:
-                throw new IllegalArgumentException("Tipo de serviço inválido");
-        }
-
-        return clientFound; // TODO - Verificar se é necessário retornar o cliente
-    }
+//    public Client getClientByCpf(String cpf){
+//        Optional<Client> clientOptional = clientDAO.findOne(cpf);
+//        if(clientOptional.isEmpty()) {
+//            throw new IllegalArgumentException("Cliente não encontrado");
+//        }
+//
+//        Client clientFound = clientOptional.get();
+//        MonthlyService monthlyService = (MonthlyService) clientFound.getService();
+//        StandardService standardService = (StandardService) clientFound.getService();
+//
+//        switch (clientFound.getService()) {
+//            case "standard": // TODO - Refatorar para usar o enum ou trocar para string
+//                // Chama o cálculo de valor do pagamento para serviço standard
+//                standardService.setCheckOut(LocalDateTime.now());
+//                standardService.calculateBilling();
+//                setPaymentChecked(true);
+//                if (isPaymentChecked()) {
+//                    // Libera a vaga e o cliente
+//                    parking.liberateAStandardParkingSpace();
+//                } else {
+//                    throw new IllegalArgumentException("Pagamento não efetuado");
+//                }
+//                break;
+//            // fluxo alternativo 1
+//            case "monthly": // TODO - Refatorar para usar o enum ou trocar para string
+//                if (monthlyService.isPaymentChecked()) {
+//                    // Libera a vaga e o cliente
+//                    parking.liberateAMonthlyParkingSpace();
+//                } else {
+//                    throw new IllegalArgumentException("Pagamento não efetuado");
+//                }
+//                break;
+//
+//            default:
+//                throw new IllegalArgumentException("Tipo de serviço inválido");
+//        }
+//
+//        return clientFound; // TODO - Verificar se é necessário retornar o cliente
+//    }
 
     public boolean isPaymentChecked() {
         return paymentChecked;
