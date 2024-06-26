ac0a3d5e2c14aba44fd8074626f83c0b7ff88256
Erick Gomes <119770693+ericksgmes@users.noreply.github.com>
2024-06-26 11:02:03 -0300

    refactor: remove wrong test file, change packages and optimize tests.

--- src/test/java/com/example/parkingsystem/domain/usecases/payment/MakePaymentUseCaseTest.java
+++ src/test/java/com/example/parkingsystem/domain/usecases/payment/MakePaymentUseCaseTest.java
@@ -3,39 +3,40 @@ package com.example.parkingsystem.domain.usecases.payment;
 import com.example.parkingsystem.domain.model.client.Client;
 import com.example.parkingsystem.domain.model.payment.Payment;
 import com.example.parkingsystem.domain.model.payment.PaymentMethodEnum;
-import org.junit.jupiter.api.Assertions;
+import com.example.parkingsystem.domain.usecases.client.MakePaymentUseCase;
 import org.junit.jupiter.api.*;
+
+import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 
 public class MakePaymentUseCaseTest {
-
-    private final Client client = new Client("123.123.123-12",
-            "A2BS2");
-    private final MakePaymentUseCase makePaymentUseCase = new MakePaymentUseCase();
-    double pricePerMinute = 0.2;
-    int timeSpentParked = 30;
+    private final com.example.parkingsystem.domain.usecases.client.MakePaymentUseCase makePayment = new MakePaymentUseCase();
+    private final Client client = new Client("123", "a123");
 
     @Test
-    @DisplayName("Should return null when client does not have entry time")
-    void shouldReturnNullWhenClientDoesNotHaveEntryTime() {
-        Assertions.assertNull(makePaymentUseCase.makePayment(client, PaymentMethodEnum.CARTAO));
+    @DisplayName("Should create a new payment")
+    void shouldCreateNewPayment() {
+        client.setEntryDate(LocalDateTime.parse("2024-05-24T08:00:00.000"));
+        Payment payment = makePayment.payment(client, PaymentMethodEnum.CARTAO);
+        Assertions.assertNotNull(payment);
     }
 
     @Test
-    @DisplayName("Should return the right amount of minutes that the client spent")
-    void shouldReturnTheRightAmountOfMinutesWhenClientSpent() {
-        client.setEntryDate(LocalDateTime.now().minusMinutes(timeSpentParked));
-        Payment payment = makePaymentUseCase.makePayment(client, PaymentMethodEnum.CARTAO);
-        Assertions.assertEquals(timeSpentParked*pricePerMinute, payment.getValue());
+    @DisplayName("Should bill correctly")
+    void shouldBillCorrectly() {
+        LocalDateTime tenMinuteEntryDate = LocalDateTime.now().minusMinutes(60);
+        client.setEntryDate(tenMinuteEntryDate);
+        Payment payment = makePayment.payment(client, PaymentMethodEnum.CARTAO);
+        Assertions.assertEquals(18,payment.getValue());
     }
 
     @Test
-    @DisplayName("Should correctly creat a payement instance")
-    void shouldCorrectlyCreatePaymentInstance() {
-        client.setEntryDate(LocalDateTime.now().minusMinutes(timeSpentParked));
-        Payment payment = makePaymentUseCase.makePayment(client, PaymentMethodEnum.DINHEIRO);
-        Assertions.assertEquals(timeSpentParked*pricePerMinute, payment.getValue());
-        Assertions.assertEquals(PaymentMethodEnum.DINHEIRO, payment.getPaymentMethod());
+    @DisplayName("Should correctly save payment method")
+    void shouldSavePaymentMethod() {
+        LocalDateTime tenMinuteEntryDate = LocalDateTime.now().minusMinutes(90);
+        client.setEntryDate(tenMinuteEntryDate);
+        Payment payment = makePayment.payment(client, PaymentMethodEnum.CARTAO);
+        Assertions.assertEquals( PaymentMethodEnum.CARTAO,payment.getPaymentMethod());
     }
 }
