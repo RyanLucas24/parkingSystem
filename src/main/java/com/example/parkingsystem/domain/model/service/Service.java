02ea94457a29fcfca99f6ff7eecce2379e27cc21
Erick Gomes <119770693+ericksgmes@users.noreply.github.com>
2024-06-26 10:52:35 -0300

    style: skip line.

--- src/main/java/com/example/parkingsystem/domain/model/service/Service.java
+++ src/main/java/com/example/parkingsystem/domain/model/service/Service.java
@@ -5,6 +5,7 @@ import com.example.parkingsystem.domain.model.client.Client;
 import java.time.LocalDateTime;
 
 public abstract class Service {
+
     public abstract double calculateBilling(LocalDateTime dataInicial);
 
     public double pricePerMinute() {
