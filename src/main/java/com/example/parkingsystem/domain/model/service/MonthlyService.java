1816464c836883d0deaf3608a1dd7833f5b846f9
Erick Gomes <119770693+ericksgmes@users.noreply.github.com>
2024-06-26 11:25:14 -0300

    style: remove unused imports.

--- src/main/java/com/example/parkingsystem/domain/model/service/MonthlyService.java
+++ src/main/java/com/example/parkingsystem/domain/model/service/MonthlyService.java
@@ -1,18 +1,13 @@
 package com.example.parkingsystem.domain.model.service;
 
-import com.example.parkingsystem.domain.model.client.Client;
-
 import java.time.LocalDateTime;
-import java.util.Date;
 
 public class MonthlyService extends Service{
-    private LocalDateTime paymentDate;
     private boolean paymentChecked = false;
 
     @Override
     public double calculateBilling(LocalDateTime dataInicial) {
         this.paymentChecked = true;
-        this.paymentDate = dataInicial;
         return price();
     }
 
